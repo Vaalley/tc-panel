a0bdd464418ce543e6697e855f17df2a61e3ff89
Valentin <61027346+Vaalley@users.noreply.github.com>
2024-05-08 12:42:42 +0200

    Initial commit + setup

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { Input } from '$lib/components/ui/input';
+</script>
+
+<svelte:head>
+	<title>🎬 Trap Cosmos Render Panel</title>
+</svelte:head>
+
+<main class="font-montserrat">
+	<h1>Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+	<Input />
+</main>
