63dc48ad47ce546776a7366a4822509dfc2351bc
Valentin <61027346+Vaalley@users.noreply.github.com>
2024-05-08 14:51:43 +0200

    Changed the css file for the new color scheme + added label on input

--- src/lib/components/ui/input/input.svelte
+++ src/lib/components/ui/input/input.svelte
@@ -4,29 +4,37 @@
 	export let value = undefined;
 	export { className as class };
 	export let readonly = undefined;
+	export let labelText = undefined;
 </script>
 
-<input
-	class={cn(
-		'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
-		className
-	)}
-	bind:value
-	{readonly}
-	on:blur
-	on:change
-	on:click
-	on:focus
-	on:focusin
-	on:focusout
-	on:keydown
-	on:keypress
-	on:keyup
-	on:mouseover
-	on:mouseenter
-	on:mouseleave
-	on:paste
-	on:input
-	on:wheel|passive
-	{...$$restProps}
-/>
+<div>
+	<label
+		class="relative left-4 top-3 bg-background p-1 text-sm font-medium text-muted-foreground"
+		for={labelText}>{labelText}</label
+	>
+	<input
+		id={labelText}
+		class={cn(
+			'flex h-10 w-full rounded-md border-2 border-input bg-background px-4 py-6 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+			className
+		)}
+		bind:value
+		{readonly}
+		on:blur
+		on:change
+		on:click
+		on:focus
+		on:focusin
+		on:focusout
+		on:keydown
+		on:keypress
+		on:keyup
+		on:mouseover
+		on:mouseenter
+		on:mouseleave
+		on:paste
+		on:input
+		on:wheel|passive
+		{...$$restProps}
+	/>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,8 +6,23 @@
 	<title>🎬 Trap Cosmos Render Panel</title>
 </svelte:head>
 
-<main class="font-montserrat">
-	<h1>Welcome to SvelteKit</h1>
-	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-	<Input />
+<main class="p-16 font-montserrat">
+	<h2
+		class="scroll-m-20 text-3xl font-medium tracking-tight text-muted-foreground transition-colors first:mt-0"
+	>
+		New Upload
+	</h2>
+	<h1 class="scroll-m-20 text-4xl font-bold tracking-tight lg:text-5xl">
+		Artist Name - Title Song (Someone Remix)
+	</h1>
+
+	<Input
+		value="https://soundcloud.com/tokzick/tupac-hit-em-up-tokzick-flip"
+		labelText="SoundCloud URL"
+	/>
+	<Input
+		value="https://unsplash.com/photos/a-boat-sitting-on-top-of-a-lake-next-to-a-mountain-3_PF89nVmuo"
+		labelText="Unsplash URL"
+	/>
+	<Input value="I'm a title" labelText="Title" />
 </main>
